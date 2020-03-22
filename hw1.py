a9540e82b17336d68e15a04eab773b08c487d339
Michał Bujniewicz-Uppal <62252280+michalduplo@users.noreply.github.com>
2020-03-22 20:41:44 +0100

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -26,9 +26,10 @@ def top5_countries_by_date(day: int, month: int, year: int = 2020) -> List[str]:
   return list(countries)
    
 
-def no_new_cases_count(day: int, month: int, year: int = 2020) -> int:
-  Dait1 = Datetime.date(year, month, day)
-  DateOneS = Dait1.strftime('%m/%d/%y').lstrip("0").replace(" 0", " ").replace("/0", "/")
-  Dait2 = Dait1 - datetime.timedelta(days=1)
-  DateTwoS = Dait2.strftime('%m/%d/%y').lstrip("0").replace(" 0", " ").replace("/0", "/")
-  return len(confirmed_cases.loc[confirmed_cases[DateOneS]-confirmed_cases[DateTwoS]!=0].index)
+
+  def no_new_cases_count(day: int, month: int, year: int = 2020) -> int:
+    Dait1 = datetime.date(year, month, day)
+    dateOneS = Dait1.strftime('%m/%d/%y').lstrip("0").replace(" 0", " ").replace("/0", "/")
+    Dait2 = Dait1 - datetime.timedelta(days=1)
+    dateTwoS = Dait2.strftime('%m/%d/%y').lstrip("0").replace(" 0", " ").replace("/0", "/")
+    return len(confirmed_cases.loc[confirmed_cases[dateOneS]-confirmed_cases[dateTwoS]!=0].index)
